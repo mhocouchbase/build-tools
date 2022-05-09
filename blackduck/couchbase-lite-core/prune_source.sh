ed73e5b49c356206694a096f0061a279aa9d000b
Ming Ho <ming.ho@couchbase.com>
2022-05-09 09:50:11 -0700

    test zlib.
    -Ming Ho

--- blackduck/couchbase-lite-core/prune_source.sh
+++ blackduck/couchbase-lite-core/prune_source.sh
@@ -10,3 +10,10 @@ rm -rf couchbase-lite-core/Xcode/LiteCore-iOS/LiteCore-iOS
 for i in fleece Docs doc test googletest third_party cbbuild tlm libstemmer_c; do
      find . -type d -name $i | xargs rm -rf
 done
+
+rm -rf couchbase-lite-core/vendor/zlib
+pushd couchbase-lite-core/vendor
+git clone https://github.com/couchbasedeps/zlib.git
+cd zlib
+git checkout v1.2.12
+popd
